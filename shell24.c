b93f99d03233c3ba48b3bd54195b3a4829196ae5
zahin <xahin96@gmail.com>
2024-03-11 01:06:14 -0400

    initial code

--- shell24.c
+++ shell24.c
@@ -4,78 +4,76 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 1024
-#define MAX_ARGUMENTS 6 // Including the command itself
-
-// Function to read user input
-char *read_input() {
-    char *input = malloc(MAX_COMMAND_LENGTH * sizeof(char));
-    if (input == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    printf("shell24$ ");
-    fgets(input, MAX_COMMAND_LENGTH, stdin);
-    return input;
-}
-
-// Function to parse input into command and arguments
-void parse_input(char *input, char *command, char *arguments[MAX_ARGUMENTS]) {
-    char *token;
-    int i = 0;
-
-    // Split input by spaces and newline
-    token = strtok(input, " \n");
-    while (token != NULL) {
-        if (i == 0) {
-            strcpy(command, token);
-        } else {
-            arguments[i - 1] = strdup(token);
-        }
-        token = strtok(NULL, " \n");
-        i++;
-    }
-    arguments[i - 1] = NULL; // Null-terminate the argument list
-}
-
-// Function to validate argc
-int is_argc_valid(int argc) {
-    return (argc >= 1 && argc <= 5);
-}
-
-// Function to execute command
-void execute_command(char *command, char *arguments[MAX_ARGUMENTS]) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Child process
-        if (execvp(command, arguments) == -1) {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // Parent process
-        wait(NULL);
-    }
-}
+#define BUFFER_LEN 1024
 
 int main() {
-    char *input;
-    char command[MAX_COMMAND_LENGTH];
-    char *arguments[MAX_ARGUMENTS];
+    char line[BUFFER_LEN];      // get command line
+    char *argv[100];            // user command
+    char *path = "/bin/";       // set path at bin
+    char progpath[20];          // full file path
+    int argc;                   // arg count
+    int special_char_count = 0; // count of special characters found
+    char special_chars[] = "|><&;#";
 
     while (1) {
-        input = read_input();
-        parse_input(input, command, arguments);
-        if (!is_argc_valid(strlen(command) + 1)) {
-            printf("Error: Number of arguments out of range (1-5).\n");
+
+        printf("My shell>> ");                    // print shell prompt
+
+        if (!fgets(line, BUFFER_LEN, stdin)) {    // get command and put it in line
+            break;                                // if user hits CTRL+D break
+        }
+
+        size_t length = strlen(line);
+        if (line[length - 1] == '\n')
+            line[length - 1] = '\0'; // Remove the newline character
+
+        if (strcmp(line, "exit") == 0) {            // check if command is exit
+            break;
+        }
+
+        char *token;                      // split command into separate strings
+        token = strtok(line, " ");
+        int i = 0;
+        while (token != NULL) {
+            argv[i] = token;
+            // Check if the token contains a special character
+            for (int j = 0; j < strlen(special_chars); j++) {
+                if (strchr(token, special_chars[j])) {
+                    special_char_count++;
+                }
+            }
+            token = strtok(NULL, " ");
+            i++;
+        }
+
+        if (special_char_count > 1) {
+            printf("Invalid command: Multiple types of special characters found\n");
+            special_char_count = 0;
             continue;
         }
-        execute_command(command, arguments);
-        free(input);
-    }
 
+        for (i = 0; i < argc; i++) {
+            printf("%s\n", argv[i]);      // print command/args
+        }
+        strcpy(progpath, path);           // copy /bin/ to file path
+        strcat(progpath, argv[0]);            // add program to path
+
+        for (i = 0; i < strlen(progpath); i++) {    // delete newline
+            if (progpath[i] == '\n') {
+                progpath[i] = '\0';
+            }
+        }
+        int pid = fork();              // fork child
+
+        if (pid == 0) {               // Child
+            execvp(progpath, argv);
+            fprintf(stderr, "Child process could not do execvp\n");
+
+        } else {                    // Parent
+            wait(NULL);
+            printf("Child exited\n");
+        }
+
+    }
     return 0;
 }
---
+++ special_character_check.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, i;
+    int count[256] = {0};
+    char *special_chars[] = {"#", "|", "<", ">", ">>", "&&", "||", ";"};
+    int num_special_chars = sizeof(special_chars) / sizeof(special_chars[0]);
+    int special_char_count = 0;
+
+    printf("Enter a command: ");
+    fgets(str, sizeof(str), stdin);
+    len = strlen(str);
+
+    // Count occurrences of each character
+    for (i = 0; i < len; i++) {
+        count[(int)str[i]]++;
+    }
+
+    // Count occurrences of special characters
+    for (i = 0; i < num_special_chars; i++) {
+        char *pos = strstr(str, special_chars[i]);
+        while (pos != NULL) {
+            special_char_count++;
+            pos = strstr(pos + 1, special_chars[i]);
+        }
+    }
+
+    if (special_char_count == 0) {
+        printf("No special character found in the command\n");
+    } else if (special_char_count == 1) {
+        printf("Special character found in the command\n");
+    } else {
+        printf("Multiple Special characters found in the command\n");
+    }
+
+    return 0;
+}
